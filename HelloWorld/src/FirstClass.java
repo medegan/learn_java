ec6b53cd2dc1deba4284589d47252d60377b6516
medegan <147056666+medegan@users.noreply.github.com>
2024-03-22 17:15:07 +0100

    Section 2

--- HelloWorld/src/FirstClass.java
+++ HelloWorld/src/FirstClass.java
@@ -1,7 +1,5 @@
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.print("Hello World");
-        System.out.println("nnnnnn");
-        System.out.println("second try");
+        System.out.println("Hello World");
     }
 }
