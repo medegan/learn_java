9b925fd7c8fb3ab2a06ec7351392269df7baa181
medegan <147056666+medegan@users.noreply.github.com>
2024-04-21 15:47:18 +0100

    update for 21st April

--- HelloWorld/src/methodOverload.java
+++ HelloWorld/src/methodOverload.java
@@ -3,6 +3,16 @@ public class methodOverload {
         double totalInches = convertToCentimeters(5,7);
         double totalCentimeter = convertToCentimeters((int)totalInches);
         System.out.println(totalCentimeter);
+
+        //convert seconds to hours
+        String convertedTime = getDurationString(7200);
+        System.out.println(convertedTime);
+
+        //convert minute and seconds to hours
+        String convertedMinuteTime = getDurationString(3200, 59);
+        System.out.println(convertedMinuteTime);
+
+
     }
 
     /*Convert height in inches to centimeters
@@ -18,5 +28,36 @@ public class methodOverload {
     public static double convertToCentimeters(int heightFeet, int heightInches){
         double totalHeightInches  = (12 * heightFeet) + heightInches;
         return  totalHeightInches;
+    }
+
+    /*Convert seconds to hour*/
+    public static String getDurationString(int seconds){
+        if (seconds < 0){
+            return "Invalid seconds "+seconds+" given";
+        }else{
+            int hour = seconds / 3600;
+            int minuteRemainder = seconds % 3600;
+            int min = 0;
+            int sec = 0;
+            if (minuteRemainder != 0){
+                min = minuteRemainder / 60;
+                sec = minuteRemainder % 60;
+            }else{
+                min = 0;
+                sec = 0;
+            }
+
+            return hour+"h "+min+"m "+sec+"s";
+        }
+    }
+
+    public static String getDurationString(int seconds, int minute){
+        if(seconds < 0 ||seconds > 59 || minute < 0){
+            return "Invalid input given";
+        }else{
+            //convert minute and seconds to seconds
+            int totalSeconds = (minute * 60)+ seconds;
+            return getDurationString(totalSeconds);
+        }
     }
 }
