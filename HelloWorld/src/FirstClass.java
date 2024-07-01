9f1a08593afc018cc62965781b9985f7dae54995
medegan <147056666+medegan@users.noreply.github.com>
2024-07-01 10:00:36 +0100

    updaye 1st July

--- HelloWorld/src/FirstClass.java
+++ HelloWorld/src/FirstClass.java
@@ -1,5 +1,24 @@
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        int number = 5;
+        int evenCount = 0;
+        int oddCount = 0;
+        while (number <= 20){
+            if(isEven(number)){
+                System.out.println(number);
+                evenCount++;
+            }else{
+                oddCount++;
+            }
+            number ++;
+        }
+       }
+
+    public static boolean isEven(int number){
+        boolean isEvenNumber = false;
+        isEvenNumber = number % 2 == 0;
+
+        return isEvenNumber;
     }
 }
